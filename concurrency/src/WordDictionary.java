b2ed2c4165b1272480d099c31baedf13e07529cc
ebbarosander <55060079+ebbarosander@users.noreply.github.com>
2019-09-30 08:45:42 +0200

    -moved file

---
+++ concurrency/src/WordDictionary.java
@@ -0,0 +1,35 @@
+
+/**
+* Class working as the model
+* Contains methods and strings representing the dictionary used for falling words
+* 
+* @author Ebba Rosander
+* 
+*/
+public class WordDictionary {
+	int size;
+	static String [] theDict= {"litchi","banana","apple","mango","pear","orange","strawberry",
+		"cherry","lemon","apricot","peach","guava","grape","kiwi","quince","plum","prune",
+		"cranberry","blueberry","rhubarb","fruit","grapefruit","kumquat","tomato","berry",
+		"boysenberry","loquat","avocado"}; //default dictionary
+	
+	WordDictionary(String [] tmp) {
+		size = tmp.length;
+		theDict = new String[size];
+		for (int i=0;i<size;i++) {
+			theDict[i] = tmp[i];
+		}
+		
+	}
+	
+	WordDictionary() {
+		size=theDict.length;
+		
+	}
+	
+	public synchronized String getNewWord() {
+		int wdPos= (int)(Math.random() * size);
+		return theDict[wdPos];
+	}
+	
+}
